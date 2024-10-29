84d461cbbdede4e71dd3496902f57ed2226123bf
JackoboLeChocobo <imbert.jacquesolivier@gmail.com>
2024-10-29 08:45:35 +0100

    Update usagi-discord.sh

--- packages/scripts/usagi-discord.sh
+++ packages/scripts/usagi-discord.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-lxterminal -e 'sudo wget "https://discord.com/api/download?platform=linux&format=deb" -O /etc/usagi/openbox-scripts/discord.deb && dpkg -i /etc/usagi/openbox-scripts/discord.deb'
+lxterminal -e 'sudo wget "https://discord.com/api/download?platform=linux&format=deb" -O /etc/usagi/openbox-scripts/discord.deb & sudo dpkg -i /etc/usagi/openbox-scripts/discord.deb'
 /usr/bin/discord
