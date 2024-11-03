0654abc2b68096f37075ebd4fabbd47240272db4
Jackobo Le Chocobo <117809035+JackoboLeChocobo@users.noreply.github.com>
2024-11-03 17:12:27 +0100

    Update apps-upscayl.sh

--- packages/apps-upscayl.sh
+++ packages/apps-upscayl.sh
@@ -11,8 +11,8 @@ echo "Vous pouvez ajouter ces informations pour configurer votre menu désormais
 echo "- Menu > Configurer Le Menu"
 echo " "
 echo "- Catégorie suggérée : Graphisme"
-echo "- Nom suggéré : Inkscape"
-echo "- Executable : /usr/bin/inkscape"
-echo "- Icone : /etc/usagi/openbox-icons/inkscape.svg"
+echo "- Nom suggéré : Upscayl"
+echo "- Executable : /etc/usagi/openbox-scripts/upscayl-2.11.5-linux.AppImage"
+echo "- Icone : /etc/usagi/openbox-icons/upscayl.png"
 echo " "
 read -p "Appuyer sur une touche pour quitter... " -n1 -s
