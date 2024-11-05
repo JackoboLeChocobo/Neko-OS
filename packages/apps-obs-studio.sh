62de5c4dc8b518362aad939e766fe02cccbe1785
JackoboLeChocobo <imbert.jacquesolivier@gmail.com>
2024-11-05 11:44:14 +0100

    OBS Studio

---
+++ packages/apps-obs-studio.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Sécurité si echec 
+apt --fix-broken install
+
+apt install obs-studio
+
+echo " "
+echo "Vous pouvez ajouter ces informations pour configurer votre menu désormais."
+echo "- Menu > Configurer Le Menu"
+echo " "
+echo "- Catégorie suggérée : Multimédia"
+echo "- Nom suggéré : OBS Studio"
+echo "- Executable : /usr/bin/obs-studio"
+echo "- Icone : /etc/usagi/openbox-icons/obs-studio.svg"
+echo " "
+read -p "Appuyer sur une touche pour quitter... " -n1 -s
